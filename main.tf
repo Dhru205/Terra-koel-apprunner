10e9fe76aca2e6706e19346c55d9f9754677f342
dhru prajapati <dhruprajapati18@gmail.com>
2022-10-25 10:31:41 +0530

    hello-7

--- main.tf
+++ main.tf
@@ -7,30 +7,23 @@ terraform {
   }
 }
 
-resource "aws_apprunner_auto_scaling_configuration_version" "dhru" {                            
-  auto_scaling_configuration_name = "dhru"
-  # scale between 1-5 containers
-  min_size = 1
-  max_size = 5
-}
+resource "aws_apprunner_service" "example" {
+  service_name = "example"
 
-resource "aws_apprunner_service" "dhru" {                            
-  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.dhru.arn                          
-         
-  service_name = "dhru"                          
-                            
-  source_configuration {                              
-    image_repository {                                
-      image_configuration {                                  
-        port = "80"                                
-      }                                
-      
-      image_identifier       = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel:latest"                                
-      image_repository_type = "ECR"                              
-    }                          
-                              
-    auto_deployments_enabled = false                            
-  }                          
-}
+  source_configuration {
+    
+    image_repository {
+      image_configuration {
+        port = "80"
+      }
+      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel:latest"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = false
+  }
 
+  tags = {
+    Name = "example-apprunner-service"
+  }
+}
 
