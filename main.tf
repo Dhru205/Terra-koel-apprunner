40692fbecb97ada2451883aacd31a5e0080f60db
dhru prajapati <dhruprajapati18@gmail.com>
2022-10-25 10:16:14 +0530

    hello-3

--- main.tf
+++ main.tf
@@ -7,23 +7,32 @@ terraform {
   }
 }
 
-resource "aws_apprunner_service" "example" {
-  service_name = "example"
+resource "aws_apprunner_auto_scaling_configuration_version" "hello" {                            
+  auto_scaling_configuration_name = "hello"
+  # scale between 1-5 containers
+  min_size = 1
+  max_size = 5
+}
 
-  source_configuration {
-    
-    image_repository {
-      image_configuration {
-        port = "80"
-      }
-      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel:latest"
-      image_repository_type = "ECR_PUBLIC"
-    }
-    auto_deployments_enabled = false
-  }
-
-  tags = {
-    Name = "example-apprunner-service"
-  }
+resource "aws_apprunner_service" "hello" {                            
+  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.hello.arn                          
+         
+  service_name = "hello-app-runner"                          
+                            
+  source_configuration {                              
+    image_repository {                                
+      image_configuration {                                  
+        port = "80"                                
+      }                                
+      
+      image_identifier       = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel:latest"                                
+      image_repository_type = "ECR"                              
+    }                          
+                              
+    auto_deployments_enabled = false                            
+  }                          
+}
+output "apprunner_service_hello" {                            
+  value = aws_apprunner_service.hello                          
 }
 
