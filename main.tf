f1f17520da16c1df234c8135e76d69ec46374af6
dhru prajapati <dhruprajapati18@gmail.com>
2022-10-26 13:43:11 +0530

    hello-36

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@
    }
  }
 
-# resource "authentication_configuration" "dhru"{
-#   access_role_arn= "arn:aws:iam::530211691714:role/ecr-role"
-# }
+resource "authentication_configuration" "dhru"{
+  access_role_arn= "arn:aws:iam::530211691714:role/ecr-role"
+}
 
 resource "aws_apprunner_service" "example" {
  
