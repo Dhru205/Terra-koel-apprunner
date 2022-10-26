d5da8aec6ad1a324546a0378d06f4749a8cffd0e
dhru prajapati <dhruprajapati18@gmail.com>
2022-10-26 13:18:52 +0530

    hello-93

--- main.tf
+++ main.tf
@@ -7,15 +7,12 @@ terraform {
   }
 }
 
-resource "aws_apprunner_auto_scaling_configuration_version" "hello" {                            
-  auto_scaling_configuration_name = "hello"
-  # scale between 1-5 containers
-  min_size = 1
-  max_size = 5
+resource "authentication_configuration"{
+  access_role_arn=arn:aws:iam::530211691714:role/ecr-role
 }
 
 resource "aws_apprunner_service" "example" {
-  auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.hello.arn
+ 
   service_name = "example"
 
   source_configuration {
@@ -24,7 +21,7 @@ resource "aws_apprunner_service" "example" {
       image_configuration {
         port = "80"
       }
-      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel:latest"
+      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel-2:latest"
       image_repository_type = "ECR"
     }
     auto_deployments_enabled = false
