f0cb61fd6d952180117a168e3ac4b32c6ef17cdb
dhru prajapati <dhruprajapati18@gmail.com>
2022-10-26 15:38:36 +0530

    Ecr image change

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_apprunner_service" "example" {
       image_configuration {
         port = "80"
       }
-      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel-2:latest"
+      image_identifier      = "530211691714.dkr.ecr.ap-northeast-1.amazonaws.com/koel-2@sha256:fdfbc3c52fe6f9cdd2f2963dc8df3f2f9bad5b26d0d8208d22d999f44c778d37"
       image_repository_type = "ECR"
     }
     auto_deployments_enabled = false
